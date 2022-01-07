726d92b7f8525c60380fd2b211452a7132442040
Gokul A <prasanthgokulgp@gmail.com>
2022-01-07 19:00:27 +0530

    User Entity

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/User.java
@@ -15,7 +15,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "users") 
-public class User { 
+public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
